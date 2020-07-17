d84a6778c7197de3231a4e46bee152e577cc3f58
egriff89 <eric.griffith42@gmail.com>
2020-07-16 21:47:09 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require("electron");
+
+// Set environment
+process.env.NODE_ENV = "development";
+
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+const isMac = process.platform === "darwin" ? true : false;
+
+let mainWindow;
+
+function createMainWindow() {
+	mainWindow = new BrowserWindow({
+		title: "ImageShrink",
+		width: 500,
+		height: 600,
+		icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+		resizable: isDev,
+	});
+
+	mainWindow.loadFile(`${__dirname}/app/index.html`);
+}
+
+app.on("ready", createMainWindow);
+
+app.on("window-all-closed", () => {
+	if (!isMac) {
+		app.quit();
+	}
+});
+
+app.on("activate", () => {
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createMainWindow();
+	}
+});
