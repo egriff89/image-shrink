3ba7d7ce202ce1f2c843df251adf9ab45a9c272b
egriff89 <eric.griffith42@gmail.com>
2020-07-18 01:40:53 -0500

    Enable node_modules browser integration

--- main.js
+++ main.js
@@ -12,11 +12,18 @@ let aboutWindow;
 function createMainWindow() {
    mainWindow = new BrowserWindow({
       title: 'ImageShrink',
-      width: 500,
+      width: isDev ? 700 : 500,
       height: 600,
       icon: `${__dirname}/assets/icons/Icon_256x256.png`,
       resizable: isDev,
+      webPreferences: {
+         nodeIntegration: true, // Enable use of node modules in browser
+      },
    });
+
+   if (isDev) {
+      mainWindow.webContents.openDevTools();
+   }
 
    mainWindow.loadFile(`${__dirname}/app/index.html`);
 }
